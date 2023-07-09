816089e23bd5ffec27451cfb04ba1b9a83a6e9d3
Francois <119730427+omelette-bio@users.noreply.github.com>
2023-07-09 17:22:24 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,17 +1,12 @@
-#! /usr/bin/env python3
-
-import os, discord, random, argparse
+import os, discord, random, argparse, json
 from blagues_api import BlaguesAPI, BlagueType
 from discord.ext import commands
 from dotenv import load_dotenv
-import json
-
-
 
 
 # récupère le mode de lancement du bot
 parser = argparse.ArgumentParser()
-parser.add_argument('mode', type=int, help='Mode de lancement du bot, 1: dev, 2: public')
+parser.add_argument('-m','--mode', type=int, default=0, help='Mode de lancement du bot, 1: dev, autre: public')
 args = parser.parse_args()
 
 
@@ -39,6 +34,26 @@ def all_char_punct(str):
          all = False
    return all
 
+def cant_write_in_dev(ctx):
+   return 'dev' not in [role.name.lower() for role in ctx.author.roles] and args.mode == 1
+
+
+def keyword_in_msg(msg, keyword):
+   assert type(msg) == list
+   for i in data['possible_'+keyword]:
+      if msg[-1] == i:
+         return True
+      elif len(msg)>1 and i in msg[-2] and all_char_punct(msg[-1]):
+            return True
+
+async def send_joke(ctx, type):
+   if cant_write_in_dev(ctx):
+      await ctx.send("le bot est en dev mode sorry :(")
+   else:
+      blague = await blagues.random_categorized(type)
+      await ctx.send(blague.joke)
+      await ctx.send(blague.answer + random.choice(data['emojis']))
+
 # display a message when the bot is ready and indicate the mode of the bot
 @bot.event
 async def on_ready():
@@ -47,44 +62,15 @@ async def on_ready():
    else:
       print(f'{bot.user.name} has connected to Discord in public mode !')
 
-
 # commande qui affiche une blague beauf
 @bot.command(name='blague_beauf')
 async def blague_beauf(ctx):
-   # si le dev mode est activé et que l'utilisateur n'a pas le role "dev", le bot ne répond pas
-   if 'dev' not in [role.name.lower() for role in ctx.author.roles] and args.mode == 1:
-      await ctx.send("le bot est en dev mode sorry :(")
-   # sinon, le bot répond
-   else:
-      blague = await blagues.random_categorized(BlagueType.BEAUF)
-      await ctx.send(blague.joke)
-      await ctx.send(blague.answer + random.choice(data['emojis']))
-
-
-# commande qui affiche une blague humour noir
-@bot.command(name='limite_limite')
-async def humour_noir(ctx):
-   # si le dev mode est activé et que l'utilisateur n'a pas le role "dev", le bot ne répond pas
-   if 'dev' not in [role.name.lower() for role in ctx.author.roles] and args.mode == 1:
-      await ctx.send("le bot est en dev mode sorry :(")
-   # sinon, le bot répond
-   else:
-      blague = await blagues.random_categorized(BlagueType.LIMIT)
-      await ctx.send(blague.joke)
-      await ctx.send(blague.answer + random.choice(data['emojis']))
+   await send_joke(ctx, BlagueType.BEAUF)
 
 # commande qui affiche une blague humour noir
 @bot.command(name='humour_noir')
 async def humour_noir(ctx):
-   # si le dev mode est activé et que l'utilisateur n'a pas le role "dev", le bot ne répond pas
-   if 'dev' not in [role.name.lower() for role in ctx.author.roles] and args.mode == 1:
-      await ctx.send("le bot est en dev mode sorry :(")
-   # sinon, le bot répond
-   else:
-      blague = await blagues.random_categorized(BlagueType.DARK)
-      await ctx.send(blague.joke)
-      await ctx.send(blague.answer + random.choice(data['emojis']))
-
+   await send_joke(ctx, BlagueType.HUMOUR_NOIR)
 
 # commande qui permet de changer le mode du bot, de dev à public et vice versa
 @bot.command(name='toggle_dev')
@@ -93,10 +79,10 @@ async def toggle_dev(ctx):
    if 'dev' in [role.name.lower() for role in ctx.author.roles]:
       if args.mode == 1:
          args.mode = 2
-         await ctx.send("> le bot est maintenant en mode public")
+         await ctx.send("le bot est maintenant en mode public")
       else:
          args.mode = 1
-         await ctx.send("> le bot est maintenant en mode dev")
+         await ctx.send("le bot est maintenant en mode dev")
    # sinon, on ne fait rien
    else:
       await ctx.send("tu n'as pas le droit de faire ça, t'es pas dev :joy_cat:")
@@ -106,7 +92,7 @@ async def toggle_dev(ctx):
 @bot.command(name='help')
 async def help(ctx):
    # si le dev mode est activé et que l'utilisateur n'a pas le role "dev", le bot ne répond pas
-   if 'dev' not in [role.name.lower() for role in ctx.author.roles] and args.mode == 1:
+   if cant_write_in_dev(ctx):
       await ctx.send("le bot est en dev mode sorry :smiling_face_with_tear: ")
    # sinon, le bot répond
    else:
@@ -121,91 +107,35 @@ async def help(ctx):
 # partie qui permet au bot de répondre à certains messages
 @bot.event
 async def on_message(message):
-   quoi = False
-   qui = False
-   hein = False
-   feur = False
-   
    # n'envoie pas de message si le message est envoyé par le bot (évite les boucles infinies)
    if message.author == bot.user:
          return
    
-   for ans in data['formulations']:
-      if ans in message.content.lower():
-         await message.reply(random.choice(data['formulations'][ans]) + " :joy_cat:")
-         return
-   
    msg = message.content.lower().split(" ")
+   keywords = ['quoi', 'qui', 'hein']
    
-   # regarde si le message fini par un des mots de la liste possible_quoi
-   for i in data['possible_quoi']:
-      if i in msg[-1]:
-         # si le dev mode est activé et que l'utilisateur n'a pas le role "dev", le bot ne répond pas
-         if 'dev' not in [role.name.lower() for role in message.author.roles] and args.mode == 1:
+   for keyword in keywords:
+      if keyword_in_msg(msg, keyword):
+         if cant_write_in_dev(message):
             return
-         quoi = True
-      elif len(msg)>1:
-         if i in msg[-2] and all_char_punct(msg[-1]):
-            # si le dev mode est activé et que l'utilisateur n'a pas le role "dev", le bot ne répond pas
-            if 'dev' not in [role.name.lower() for role in message.author.roles] and args.mode == 1:
-               return
-            quoi = True
-
-   # regarde si le message fini par un des mots de la liste possible_qui
-   for i in data['possible_qui']:
-      if i in msg[-1]:
-         # si le dev mode est activé et que l'utilisateur n'a pas le role "dev", le bot ne répond pas
-         if 'dev' not in [role.name.lower() for role in message.author.roles] and args.mode == 1:
-            return
-         qui = True
-      elif len(msg)>1:
-         if i in msg[-2] and all_char_punct(msg[-1]):
-            # si le dev mode est activé et que l'utilisateur n'a pas le role "dev", le bot ne répond pas
-            if 'dev' not in [role.name.lower() for role in message.author.roles] and args.mode == 1:
-               return
-            qui = True
-      
-   for i in data["possible_hein"]:
-      if i in msg[-1]:
-         # si le dev mode est activé et que l'utilisateur n'a pas le role "dev", le bot ne répond pas
-         if 'dev' not in [role.name.lower() for role in message.author.roles] and args.mode == 1:
-            return
-         hein = True
-      elif len(msg)>1:
-         if i in msg[-2] and all_char_punct(msg[-1]):
-            # si le dev mode est activé et que l'utilisateur n'a pas le role "dev", le bot ne répond pas
-            if 'dev' not in [role.name.lower() for role in message.author.roles] and args.mode == 1:
-               return
-            hein = True
-   
+         await message.reply(random.choice(data['answers_'+keyword]) + " :joy_cat:")
+         return
+         
    #check if a message contain "feur" and send a message
    for i in data['answers_quoi']:
       if i in message.content.lower():
          # si le dev mode est activé et que l'utilisateur n'a pas le role "dev", le bot ne répond pas
          if 'dev' not in [role.name.lower() for role in message.author.roles] and args.mode == 1:
             return
-         feur = True
+         # sinon, le bot répond
+         await message.add_reaction('🙀')
+         await message.reply("masterclass akhy :joy_cat:")
+         return # on sort de la fonction pour éviter que le bot réponde deux fois
 
-   if quoi and feur:
-      await message.reply("Masterclass, mais mange quand même ton feur :joy_cat:")
-      return
-   
-   elif quoi:
-      await message.reply(random.choice(data['answers_quoi']) + " :joy_cat:")
-      return # on sort de la fonction pour éviter que le bot réponde deux fois
-   
-   elif qui:
-      await message.reply("quette :joy_cat:")
-      return
-   
-   elif hein:
-      await message.reply(random.choice(data['answers_hein']) + " :joy_cat:")
-      return
-   
-   elif feur:
-      await message.add_reaction('🙀')
-      await message.reply("masterclass akhy :joy_cat:")
-      return # on sort de la fonction pour éviter que le bot réponde deux fois 
+   for ans in data['formulations']:
+      if ans in message.content.lower():
+         await message.reply(data["formulations"][ans] + " :joy_cat:")
+         return # on sort de la fonction pour éviter que le bot réponde deux fois
    
    # regarde si le message est une commande
    await bot.process_commands(message)
